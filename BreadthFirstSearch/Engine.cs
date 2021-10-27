42d839e79a2fc0088a33225424e59d4efe402e8a
Paula Fahmy <paula.fahmy@flairstech.com>
2021-10-27 17:44:22 +0200

    Implemented main search engine logic

--- BreadthFirstSearch/Engine.cs
+++ BreadthFirstSearch/Engine.cs
@@ -1,13 +1,75 @@
-﻿using System;
+﻿using BreadthFirstSearch.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BreadthFirstSearch
 {
     public class Engine
     {
+        private Queue<Node> mainQueue;
+        private Node startingNode;
+        private Node endingNode;
+        private readonly List<Node> allNodes;
 
+        public Engine(List<Node> allNodes)
+        {
+            this.allNodes = allNodes;
+
+            mainQueue = new Queue<Node>();
+
+            startingNode = allNodes.First();
+            endingNode = allNodes.Last();
+        }
+
+        public void Discover()
+        {
+            VisitNode(startingNode);
+
+            while(mainQueue.TryDequeue(out Node currentNode))
+            {
+                // Get neighbor as a node from allNodes (instead of using simple string)
+                var neighbors = allNodes.Where(n => currentNode.Neighbors.Any(cn => cn == n.Name)).ToList();
+
+                foreach(var neighbor in neighbors)
+                {
+                    if (!neighbor.Visited)
+                    {
+                        VisitNode(neighbor, currentNode);
+                    }
+                }
+            }
+        }
+
+        public string Solve()
+        {
+            var nearestPath = new List<string>();
+
+            nearestPath.Add(endingNode.Name);
+
+            var previousNode = endingNode.PreviousNode;
+            
+            while (previousNode != null)
+            {
+                nearestPath.Add(previousNode.Name);
+
+                previousNode = previousNode.PreviousNode;
+            }
+
+            if (nearestPath.Count == 1)
+                return "No Path";
+            else
+            {
+                nearestPath.Reverse();
+                return string.Join("-", nearestPath);
+            }
+        }
+
+        private void VisitNode(Node node, Node previousNode = null)
+        {
+            node.PreviousNode = previousNode;
+            node.Visited = true;
+
+            mainQueue.Enqueue(node);
+        }
     }
 }
